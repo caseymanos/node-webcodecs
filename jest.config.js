894043b7f44bccf3794b88662318ae71bc06f9e4
Casey Manos <manoscasey@gmail.com>
2025-12-30 03:35:37 -0500

    Fix CI: exclude spec-compliance tests from Jest (run via vitest)

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/test/spec-compliance/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverageFrom: [
     'src/**/*.ts',
